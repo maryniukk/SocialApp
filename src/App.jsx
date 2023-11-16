7e97235c194b3473ac03b07a4ced56e047aa0da7
Nikita <nikitamaryniuk@gmail.com>
2023-11-16 18:29:19 +0100

    commit 16.11

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,22 @@ function App() {
 		<div class='mx-auto max-w-5xl'>
 			<Header />
 			<div class='flex'>
-				<nav class='text-2xl pt-5'>
-					<div>Profile</div>
-					<div>Messages</div>
-					<div>News</div>
-					<div>Music</div>
-					<div class='pt-2'>Settings</div>
+				<nav class='text-2xl pt-5 	'>
+					<div class=''>
+						<div>Profile</div>
+						<div>Messages</div>
+						<div>News</div>
+						<div>Music</div>
+						<div class='pt-2'>Settings</div>
+					</div>
 				</nav>
-				<div class='flex'>Main content</div>
+				<div class='flex pl-40 pt-5'>
+					<img
+						class='rounded-md'
+						src='https://i.pinimg.com/originals/6c/73/75/6c737506872effa6caac3d6a20ba2b53.jpg'
+						width={500}
+					/>
+				</div>
 			</div>
 		</div>
 	)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 
 const Navbar = () => {
 	return (
-		<nav class='text-base'>
-			<div>Profile</div>
-			<div>Messages</div>
-			<div>News</div>
-			<div>Music</div>
-			<div>Settings</div>
-		</nav>
+		<div class='flex'>
+			<nav class='text-2xl pt-5 	'>
+				<div class=''>
+					<div>Profile</div>
+					<div>Messages</div>
+					<div>News</div>
+					<div>Music</div>
+					<div class='pt-2'>Settings</div>
+				</div>
+			</nav>
+			<div class='flex pl-40 pt-5'>
+				<img
+					class='rounded-md'
+					src='https://i.pinimg.com/originals/6c/73/75/6c737506872effa6caac3d6a20ba2b53.jpg'
+					width={500}
+				/>
+			</div>
+		</div>
 	)
 }
 
