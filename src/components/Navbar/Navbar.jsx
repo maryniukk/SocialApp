e5be21fb8bb4275ac63e6be2d517494ab3623899
Nikita <nikitamaryniuk@gmail.com>
2023-11-22 16:34:23 +0100

    routing just created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,21 @@ const Navbar = () => {
 		<div class='flex'>
 			<nav class='text-xl pt-5'>
 				<div class=''>
-					<div class='pb-1'>Profile</div>
-					<div class=' text-blue-500 text-3xl font-medium pb-1'>Messages</div>
-					<div class='pb-1'>News</div>
-					<div class='pb-1'>Music</div>
-					<div class='pb-2'>Settings</div>
+					<div class='pb-1'>
+						<a>Profile</a>
+					</div>
+					<div class=' text-blue-500 text-3xl font-medium pb-1'>
+						<a href='#!'>Messages</a>
+					</div>
+					<div class='pb-1'>
+						<a href='#!'>News</a>
+					</div>
+					<div class='pb-1'>
+						<a href='#!'>Music</a>
+					</div>
+					<div class='pb-2'>
+						<a href='#!'>Settings</a>
+					</div>
 				</div>
 			</nav>
 			<NavbarImg />
