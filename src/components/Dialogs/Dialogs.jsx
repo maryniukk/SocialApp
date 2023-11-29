94a6167eeb5159541cbff285d2a37e59fdea1b3f
Nikita <nikitamaryniuk@gmail.com>
2023-11-29 17:03:54 +0100

    29.11 v1.0

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-let dialogsData = [
-	{ id: 1, name: 'Nikita' },
-	{ id: 2, name: 'Taras' },
-	{ id: 3, name: 'Kirill' },
-	{ id: 4, name: 'Danya' },
-]
-let messages = [
-	{ id: 1, message: 'Hi!' },
-	{ id: 2, message: 'How are you?' },
-	{ id: 3, message: 'See you soon.' },
-	{ id: 4, message: 'How is your day going?' },
-]
-
 const DialogItem = props => {
 	let path = '/Dialogs/' + props.id
 	return (
@@ -32,7 +19,20 @@ const Message = props => {
 	return <div className='text-xl pb-1'>{props.message}</div>
 }
 
-let dialogsElements = dialogsData.map(dialogs => (
+let dialogsData = [
+	{ id: 1, name: 'Nikita' },
+	{ id: 2, name: 'Taras' },
+	{ id: 3, name: 'Kirill' },
+	{ id: 4, name: 'Danya' },
+]
+let messages = [
+	{ id: 1, message: 'Hi!' },
+	{ id: 2, message: 'How are you?' },
+	{ id: 3, message: 'See you soon.' },
+	{ id: 4, message: 'How is your day going?' },
+]
+
+let dialogsElements = dialogsData.map(d => (
 	<DialogItem name={d.name} id={d.id} />
 ))
 let messagesElements = messages.map(m => <Message message={m.message} />)
@@ -42,12 +42,7 @@ const Dialogs = () => {
 		<div className='flex pl-20 pt-5'>
 			<div>{dialogsElements}</div>
 			<div>
-				<div className='pl-20'>
-					<Message message={messagesData[0].message} />
-					<Message message={messagesData[1].message} />
-					<Message message={messagesData[2].message} />
-					<Message message={messagesData[3].message} />
-				</div>
+				<div className='pl-20'>{messagesElements}</div>
 			</div>
 		</div>
 	)
