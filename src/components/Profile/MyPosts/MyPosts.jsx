36fd9cc631e46f14fd1b56fb7555c63a801f1539
Nikita <nikitamaryniuk@gmail.com>
2023-12-03 14:44:59 +0100

    03.12 commit

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import Post from '../Post'
-const MyPosts = () => {
-	let posts = [
-		{ id: 1, message: 'Hi, how are you?', likesCount: 12 },
-		{ id: 2, message: 'It is my first post!', likesCount: 11 },
-		{ id: 3, message: 'Blabla', likesCount: 50 },
-		{ id: 4, message: 'Lalalala', likesCount: 5 },
-	]
-	let postElements = posts.map(p => (
+const MyPosts = props => {
+	let postElements = props.posts.map(p => (
 		<Post key={[p.id]} message={p.message} likesCount={p.likesCount} />
 	))
 
--- src/components/Profile/ProfileInfo/Profile.jsx
+++ src/components/Profile/ProfileInfo/Profile.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import MyPosts from '../MyPosts/MyPosts'
 import ProfileInfo from './ProfileInfo'
 const Profile = () => {
+	let posts = [
+		{ id: 1, message: 'Hi, how are you?', likesCount: 12 },
+		{ id: 2, message: 'It is my first post!', likesCount: 11 },
+		{ id: 3, message: 'Blabla', likesCount: 50 },
+		{ id: 4, message: 'Lalalala', likesCount: 5 },
+	]
 	return (
 		<div className='pl-20'>
 			<ProfileInfo />
-			<MyPosts />
+			<MyPosts posts={posts} />
 		</div>
 	)
 }
