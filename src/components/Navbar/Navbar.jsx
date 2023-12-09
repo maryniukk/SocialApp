131764867119eacf227e1b3a3219da34998dcf97
Nikita <nikitamaryniuk@gmail.com>
2023-12-09 18:38:05 +0100

    09.12

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 					<div>
 						<div className='pb-2 hover:text-gray-700 transition-all'>
 							<NavLink to='/Profile' className={isActive('/Profile')}>
-								Profile
+								Profile123
 							</NavLink>
 						</div>
 						<div className='pb-2  hover:text-gray-700 transition-all'>
