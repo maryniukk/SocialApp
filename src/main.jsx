040f3e459633e479b369994e4573f003ae8242a6
Nikita <nikitamaryniuk@gmail.com>
2023-12-14 11:43:54 +0100

    14.12

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import state, { addPost } from './redux/state.js'
 
+addPost('SamuraiJs.com')
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App appState={state} addPost={addPost} />
